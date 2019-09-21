951c623750f68c54d22c214a9312c69559d15fb5
Alexander Klimov <gizmoptz@gmail.com>
2019-09-21 23:40:05 +0300

    Extract constants, get rid of global servers array

--- commands/list.js
+++ commands/list.js
@@ -1,8 +1,7 @@
 const Discord = require('discord.js');
 const cheerio = require('cheerio');
 const rp = require('request-promise');
-
-servers = [];
+const constants = require('../constants');
 
 function parseImage(td) {
 	return td.find('img').attr('src');
@@ -59,56 +58,37 @@ function parseInformation(td) {
 	};
 }
 
-function makePlayers(server) {
-	return server.current + '/' + server.maximum;
+function renderPlayerCount(server) {
+	return `${server.current}/${server.maximum}`;
 }
 
-function makeMinimum(server) {
-	if (server.mode == 'AIR ASSAULT') {
-		return 20;
-	} else if (server.mode == 'CONQUEST') {
-		return 20;
-	} else if (server.mode == 'DOMINATION') {
-		return 10;
-	} else if (server.mode == 'FRONTLINES') {
-		return 16;
-	} else if (server.mode == 'OPERATIONS') {
-		return 20;
-	} else if (server.mode == 'RUSH') {
-		return 10;
-	} else if (server.mode == 'SHOCK OPERATIONS') {
-		return 20;
-	} else if (server.mode == 'SUPPLY DROP') {
-		return 10;
-	} else if (server.mode == 'TEAM DEATHMATCH') {
-		return 10;
-	} else if (server.mode == 'WAR PIGEONS') {
-		return 10;
-	} else {
-		return 0;
-	}
+function renderMinimumPlayerCount(server) {
+	const mode = constants.gameModes.find((mode) => mode.name === server.mode);
+	return (mode && mode.minPlayerCount) || 0;
 }
 
 function makeColor(server) {
-	if (server.minimum == 0) return 'DEFAULT';
-	if (server.current == 0) {
-		return 'RED';
+	if (server.minimum === 0) return constants.colors.default;
+	if (server.current === 0) {
+		return constants.colors.red;
 	} else if (server.current <= server.minimum) {
-		return 'ORANGE';
+		return constants.colors.orange;
 	} else if (server.current <= server.maximum) {
-		return 'GREEN';
+		return constants.colors.green;
 	}
 }
 
 function parse(body) {
 	const $ = cheerio.load(body);
+	let servers = [];
 	$('tbody tr').each(function (index, element) {
 		var td = $(this).children('td');
 		var server = parseInformation(td);
 		server.id = parseInt($(this).attr('data-url').split('/').pop());
-		server.minimum = makeMinimum(server);
+		server.minimum = renderMinimumPlayerCount(server);
 		servers.push(server);
 	});
+	return servers;
 }
 
 module.exports = {
@@ -118,25 +98,33 @@ module.exports = {
 	execute(message, args) {
 		// Search for the name gRndpjv because it is the Discord Invitation Code
 		rp('https://battlefieldtracker.com/bf1/servers?platform=pc&name=gRndpjv')
-			.then(function (body) {
-				parse(body);
+			.then((body) => {
+				return new Promise((resolve, reject) => {
+					try {
+						const parsed = parse(body);
+						return resolve(parsed);
+					} catch (e) {
+						reject(e);
+					}
+				});
 			})
-			.finally(function () {
-				servers.sort(function (lhs, rhs) {
-					return lhs.number - rhs.number;
-				});
-				console.log(servers);
-				servers.forEach(function (server) {
-					const embed = new Discord.RichEmbed()
-						.setColor(makeColor(server))
-						.setThumbnail(server.image)
-						.addField('#', server.number, true)
-						.addField('Players', makePlayers(server), true)
-						.addField('Map', server.map, true)
-						.addField('Mode', server.mode, true)
-					message.channel.send(embed);
-				});
-				servers = [];
+			.then((servers) => {
+				servers
+					.sort((lhs, rhs) => lhs.number - rhs.number)
+					.forEach((server) => {
+						const embed = new Discord.RichEmbed()
+							.setColor(makeColor(server))
+							.setThumbnail(server.image)
+							.addField('#', server.number, true)
+							.addField('Players', renderPlayerCount(server), true)
+							.addField('Map', server.map, true)
+							.addField('Mode', server.mode, true);
+						
+						message.channel.send(embed);
+					});
+			})
+			.catch((err) => {
+				console.error(err);
 			});
 	},
 };
